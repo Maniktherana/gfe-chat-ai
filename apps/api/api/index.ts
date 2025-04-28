d52231304116d9c951cd2322d7c4dbf61c35eeaf
Manik Rana <manikrana54@gmail.com>
2025-04-28 15:51:17 +0530

    chore: remove runtime

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -5,10 +5,6 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 import { db } from "@repo/database/index";
 import { chatRouter } from "./routes/user/chat.index";
 
-export const config = {
-  runtime: "nodejs",
-};
-
 const app = new OpenAPIHono().route("/", chatRouter);
 
 app.get("/", (c) => {
